54579ac33f30bc76b9ee90b1c1e15e2cdb576b72
Alex Devero <deveroalex@gmail.com>
2022-09-27 15:02:39 +0200

    Add more logging

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,8 @@ export default function middleware(req: NextRequest) {
   const { device } = userAgent(req)
 
   console.log('device: ', device)
+  console.log('req: ', req)
+  console.log('user-agent: ', req.headers.get('user-agent'))
 
   const deviceType = device.type === 'desktop' ? 'desktop' : 'mobile'
 
