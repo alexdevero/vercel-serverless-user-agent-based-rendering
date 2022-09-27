8594f500413ab79df219b83d158ff69f818a7d99
Alex Devero <deveroalex@gmail.com>
2022-09-27 14:51:27 +0200

    Add device logging

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,8 @@ export const config = {
 export default function middleware(req: NextRequest) {
   const { device } = userAgent(req)
 
+  console.log('device: ', device)
+
   const deviceType = device.type === 'desktop' ? 'desktop' : 'mobile'
 
   req.nextUrl.pathname = `_viewport/${deviceType}`
